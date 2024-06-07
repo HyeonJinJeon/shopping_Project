569924e964c485432561321cc58abde58b857389
h_jinnny <nd3807@naver.com>
2024-06-08 02:52:44 +0900

    상품 리스트 가져오기

--- src/main/java/com/example/shoppingproject/service/ProductService.java
+++ src/main/java/com/example/shoppingproject/service/ProductService.java
@@ -1,4 +1,23 @@
 package com.example.shoppingproject.service;
 
+import com.example.shoppingproject.domain.Product;
+import com.example.shoppingproject.repository.ProductRepository;
+import com.example.shoppingproject.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        // 예시로 더미 데이터를 생성하여 반환합니다.
+        List<Product> products = new ArrayList<>();
+        products = productRepository.findAll();
+        return products;
+    }
 }
