334caae6bbfd838deaeaf267293be2901e5c553f
h_jinnny <nd3807@naver.com>
2024-06-10 09:37:24 +0900

    카테고리 Repo 생성

--- src/main/java/com/example/shoppingproject/repository/CategoryRepository.java
+++ src/main/java/com/example/shoppingproject/repository/CategoryRepository.java
@@ -2,6 +2,10 @@ package com.example.shoppingproject.repository;
 
 import com.example.shoppingproject.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
