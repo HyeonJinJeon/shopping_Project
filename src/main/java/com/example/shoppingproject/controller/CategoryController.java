80300fbb7becc55b5846015a37b936c4e10ba93f
h_jinnny <nd3807@naver.com>
2024-06-10 09:36:16 +0900

    카테고리 리스트 view에 넘겨주기

--- src/main/java/com/example/shoppingproject/controller/CategoryController.java
+++ src/main/java/com/example/shoppingproject/controller/CategoryController.java
@@ -1,4 +1,23 @@
 package com.example.shoppingproject.controller;
 
+import org.springframework.ui.Model;
+import com.example.shoppingproject.domain.Category;
+import com.example.shoppingproject.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
 public class CategoryController {
+    private final CategoryService categoryService;
+
+    @GetMapping("/main")
+    public String main(Model model) {
+        List<Category> categories = categoryService.getAllCategories();
+        model.addAttribute("categories", categories);
+        return "category/main";
+    }
 }
