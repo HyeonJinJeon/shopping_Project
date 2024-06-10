2a59abc67efb2f75d0712c7de1bff469522fa946
h_jinnny <nd3807@naver.com>
2024-06-10 09:38:56 +0900

    Repo 어노테이션 추가

--- src/main/java/com/example/shoppingproject/repository/UserRepository.java
+++ src/main/java/com/example/shoppingproject/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.shoppingproject.repository;
 
 import com.example.shoppingproject.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 }
