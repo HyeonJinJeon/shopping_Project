dee062465f2e262a4067a2156a4516615708607c
h_jinnny <nd3807@naver.com>
2024-06-10 09:36:59 +0900

    카테고리 전체 리스트 받아오기

--- src/main/java/com/example/shoppingproject/service/CategoryService.java
+++ src/main/java/com/example/shoppingproject/service/CategoryService.java
@@ -1,4 +1,17 @@
 package com.example.shoppingproject.service;
 
+import com.example.shoppingproject.domain.Category;
+import com.example.shoppingproject.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+    private final CategoryRepository categoryRepository;
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
 }
